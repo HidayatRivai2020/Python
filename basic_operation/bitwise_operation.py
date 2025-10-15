7e91b72e5a992afd087707c56a539f7e76f7eab5
Hidayat Riva'i <dayat.hidayat.rivai@gmail.com>
2025-10-15 11:38:18 +0700

    basic operation

---
+++ basic_operation/bitwise_operation.py
@@ -0,0 +1,21 @@
+first_one = 1
+first_zero = 0
+second_one = 1
+second_zero = 0
+
+
+
+print(first_one & second_zero)
+print(first_one & second_one)
+
+print(first_zero | second_zero)
+print(first_one | second_one)
+
+print(first_zero ^ second_zero)
+print(first_one ^ second_zero)
+
+print(~first_zero)
+print(~first_one)
+
+print(12 << 3)
+print(12 >> 2)
